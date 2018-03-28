10850cfeeb1eb4bb876d59125f2be71b9ee8160c
Adam Romanek <aromanek.contractor@libertyglobal.com>
2018-03-28 12:11:24 +0200

    Fixed -c/-C/-k command line flags (incorrect KPF_* constants usage)
    In the upstream repo commit 9eed624c760b9c70de9a8f75ff84c5f085d62fdc
    the original constants defined in libpagemap (PM_PAGE_*) were replaced
    by constants provided by a Linux kernel header (KPF_*). However these
    new contants are bit-shift values while the previous ones were
    bit-shifted flags (e.g 14 vs (1<<14) for
    KPF_SWAPBACKED/PM_PAGE_SWAPBACKED respectively).
    
    Upstream-Status: Submitted [via email to the committer who broke it]

--- libpagemap/pm_map.c
+++ libpagemap/pm_map.c
@@ -109,7 +109,7 @@ int pm_map_workingset(pm_map_t *map, pm_memusage_t *ws_out) {
                                 &flags);
         if (error) goto out;
 
-        if (!(flags & KPF_REFERENCED))
+        if (!(flags & (1 << KPF_REFERENCED)))
             continue;
 
         error = pm_kernel_count(map->proc->ker, PM_PAGEMAP_PFN(pagemap[i]),
